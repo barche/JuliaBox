77fbda3ec207c26dfc4d4ddfc45ebec3aa0249d3
Bart Janssens <bart@bartjanssens.org>
2017-06-28 12:25:35 +0200

    Update notebook install

--- container/interactive/IJulia/ipython_notebook_config.py
+++ container/interactive/IJulia/ipython_notebook_config.py
@@ -4,3 +4,5 @@ c.NotebookApp.open_browser = False
 c.KernelRestarter.time_to_dead = 6.0
 c.NotebookApp.ip = "*"
 c.NotebookApp.allow_origin = "*"
+c.NotebookApp.token = ''
+c.NotebookApp.password = ''
