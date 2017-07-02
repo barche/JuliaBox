bd07e5515153db9edf98b7aa12ee3537d7fc6702
Bart Janssens <bart@bartjanssens.org>
2017-07-02 22:00:36 +0200

    Checkout for ColorBrewer

--- container/interactive/setup_julia.sh
+++ container/interactive/setup_julia.sh
@@ -36,10 +36,10 @@ function list_packages {
 }
 
 # Install packages for Julia 0.5 and 0.6
-DEFAULT_PACKAGES="IJulia PyPlot Interact Colors SymPy PyCall Plots TikzPictures GR SimJulia Unitful PlotlyJS PGFPlots StaticArrays BenchmarkTools ForwardDiff"
+DEFAULT_PACKAGES="IJulia PyPlot Interact Colors ColorBrewer SymPy PyCall Plots TikzPictures GR SimJulia Unitful PlotlyJS PGFPlots StaticArrays BenchmarkTools ForwardDiff"
 INTERNAL_PACKAGES="https://github.com/tanmaykm/JuliaBoxUtils.jl.git"
 BUILD_PACKAGES="JuliaBoxUtils IJulia PyPlot"
-CHECKOUT_PACKAGES=""
+CHECKOUT_PACKAGES="ColorBrewer"
 
 for ver in 0.5 0.6
 do
